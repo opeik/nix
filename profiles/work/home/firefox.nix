c2db16600d63fbbe5caf4d9122954ad7f4623a8f
Sandro Stikić <sandro@stikic.com>
2021-11-11 17:04:20 +0800

    Update firefox config

--- profiles/work/home/firefox.nix
+++ profiles/work/home/firefox.nix
@@ -1,7 +1,32 @@
-{ pkgs, lib, ... }: {
+# Annoying hack, see: https://github.com/NixOS/nixpkgs/issues/87895#issuecomment-898191204.
+{ pkgs, lib, ... }:
+let
+  myFirefox = (pkgs.firefox.overrideAttrs (_: {
+    desktopItem =
+      pkgs.makeDesktopItem {
+        name = "firefox";
+        exec = "env MOZ_ENABLE_WAYLAND=1 MOZ_DBUS_REMOTE=1 firefox %u";
+        icon = "firefox";
+        comment = "";
+        desktopName = "Firefox";
+        genericName = "Web Browser";
+        categories = "Network;WebBrowser;";
+        mimeType = lib.concatStringsSep ";" [
+          "text/html"
+          "text/xml"
+          "application/xhtml+xml"
+          "application/vnd.mozilla.xul+xml"
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+          "x-scheme-handler/ftp"
+        ];
+      };
+  }));
+in
+{
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-wayland;
+    package = myFirefox;
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
       onepassword-password-manager
