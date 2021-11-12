1bd3b9a356abdc572cb9f34c62e2667f7d770cdb
Sandro Stikić <sandro@stikic.com>
2021-11-12 14:37:07 +0800

    Update firefox config

--- profiles/work/home/firefox.nix
+++ profiles/work/home/firefox.nix
@@ -30,6 +30,7 @@ in
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
       onepassword-password-manager
+      multi-account-containers
     ];
     profiles.opeik.settings = {
       # Restore last session on startup.
